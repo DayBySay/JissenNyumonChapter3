3dd41de6b24ffebef6ff2e2ed6b25e91d416030e
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-02-17 19:10:56 +0900

    ゲームの機能を色々追加

---
+++ Assets/SCripts/FallinChecker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallinChecker : MonoBehaviour {
+
+	public Hole red;
+	public Hole blue;
+	public Hole green;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		string label = "";
+
+		if (red.isFallIn () && blue.isFallIn () && green.isFallIn ()) {
+			label = "Fall in hole!";
+		}
+
+		GUI.Label (new Rect (0, 0, 100, 30), label);
+	}
+}
